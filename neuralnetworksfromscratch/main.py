cc45c93fb7a915c6974d7ddfc4d5379d908e31f0
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-27 20:09:18 +0100

    Layer of neurons using numpy

--- neuralnetworksfromscratch/main.py
+++ neuralnetworksfromscratch/main.py
@@ -1,23 +1,13 @@
+import numpy as np
+
 inputs = [1.0, 2.0, 3.0, 2.5]
 
 weights = [[0.2, 0.8, -0.5, 1],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2, 3, 0.5]
+biases = [2.0, 3.0, 0.5]
 
-# Output of current layer
-layer_outputs = []
-# For each neuron
-for neuron_weights, neuron_bias in zip(weights, biases):
-    # Zeroed output of given neuron
-    neuron_output = 0
-    # For each input and weight to the neuron
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-
-    neuron_output += neuron_bias
-
-    layer_outputs.append(neuron_output)
+layer_outputs = np.dot(weights, inputs) + biases
 
 print(layer_outputs)
