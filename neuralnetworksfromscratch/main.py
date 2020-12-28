dabbffb9e0b025aef2dc4638d3404e3882805881
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-28 10:32:10 +0100

    Adding a second hidden layer

--- neuralnetworksfromscratch/main.py
+++ neuralnetworksfromscratch/main.py
@@ -4,12 +4,22 @@ inputs = [[1.0, 2.0, 3.0, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5, 2.7, 3.3, -0.8]]
 
+# Hidden layer 1
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 
 biases = [2.0, 3.0, 0.5]
 
-layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+# Hidden layer 2
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
 
-print(layer_outputs)
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
