2a5ab00d251a61ee5533cd9d3494ccc6300bf376
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-29 17:30:24 +0100

    Creating Loss class

--- neuralnetworksfromscratch/main.py
+++ neuralnetworksfromscratch/main.py
@@ -40,6 +40,18 @@ class ActivationSoftmax:
         self.output = probabilities
 
 
+# Common loss class
+class Loss:
+    # Calculates the data and regularization losses given model output and ground truth values
+    def calculate(self, output, y):
+        # Calculate sample losses
+        sample_losses = self.forward(output, y)
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+        # Return loss
+        return data_loss
+
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 
