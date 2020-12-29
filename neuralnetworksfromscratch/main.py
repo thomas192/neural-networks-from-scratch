81dd1d5dbfb5a317bc00a35e8311c72c4e4d6269
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-29 17:47:21 +0100

    Creating LossCategoricalCrossentropy class

--- neuralnetworksfromscratch/main.py
+++ neuralnetworksfromscratch/main.py
@@ -46,10 +46,37 @@ class Loss:
     def calculate(self, output, y):
         # Calculate sample losses
         sample_losses = self.forward(output, y)
+
         # Calculate mean loss
         data_loss = np.mean(sample_losses)
+
         # Return loss
         return data_loss
+
+
+# Cross-entropy loss
+class LossCategoricalCrossentropy(Loss):
+    # Forward pass
+    def forward(self, y_pred, y_true):
+        # Number of samples in a batch
+        samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values -
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 
 # Create dataset
