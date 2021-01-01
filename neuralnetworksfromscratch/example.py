33b3dadfc29a6e618a924195b101458b3ce72a92
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-01-01 18:49:11 +0100

    Optimization of a single neuron

--- neuralnetworksfromscratch/example.py
+++ neuralnetworksfromscratch/example.py
@@ -16,13 +16,15 @@ z = xw0 + xw1 + xw2 + b
 # ReLU activation function
 y = max(z, 0)
 
+print("ReLU output: ", y)
+
 # Backward pass
 # The derivative from the next layer
 dvalue = 1.0
 
 # Derivative of ReLU and the chain rule
 drelu_dz = dvalue * (1. if z > 0 else 0.)
-print(drelu_dz)
+# print(drelu_dz)
 
 # Partial derivatives of the multiplication, the chain rule
 dsum_dxw0 = 1
@@ -34,7 +36,7 @@ drelu_dxw1 = drelu_dz * dsum_dxw1
 drelu_dxw2 = drelu_dz * dsum_dxw2
 drelu_db = drelu_dz * dsum_db
 
-print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+# print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
 
 # Partial derivatives of the multiplication, the chain rule
 dmul_dx0 = w[0]
@@ -50,4 +52,31 @@ drelu_dw1 = drelu_dxw1 * dmul_dw1
 drelu_dx2 = drelu_dxw2 * dmul_dx2
 drelu_dw2 = drelu_dxw2 * dmul_dw2
 
-print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+dx = [drelu_dx0, drelu_dx1, drelu_dx2]  # gradients on inputs
+dw = [drelu_dw0, drelu_dw1, drelu_dw2]  # gradients on weights
+db = drelu_db  # gradient on bias
+
+# print(dx, dw, db)
+
+print("Weights and bias before optimization: ", w, b)
+
+# Apply a fraction of the gradients to these values
+w[0] += -0.001 * dw[0]
+w[1] += -0.001 * dw[1]
+w[2] += -0.001 * dw[2]
+b += -0.001 * db
+
+print("Weights and bias after optimization: ", w, b)
+
+
+# Multiplying inputs by weights
+xw0 = x[0] * w[0]
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+
+# Adding
+z = xw0 + xw1 + xw2 + b
+
+# ReLU activation function
+y = max(z, 0)
+print("ReLU output: ", y)
